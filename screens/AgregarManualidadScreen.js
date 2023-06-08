174496b0cdb377a188d602ae380d0df2e2e36696
Josebeap <josebeap99@gmail.com>
2023-06-08 14:20:39 -0500

    Organización codigo main

--- screens/AgregarManualidadScreen.js
+++ screens/AgregarManualidadScreen.js
@@ -1,5 +1,6 @@
-import { View, Text, Modal, TextInput,
-     Picker, TouchableOpacity, Image, ScrollView, CheckBox, Alert } from "react-native";
+import { View, Text, Modal, TextInput, TouchableOpacity, Image, ScrollView, Alert } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { CheckBox } from '@react-native-community/checkbox';
 import React, { useEffect, useState } from "react";
 import { imageFunctions } from "../generalFunctions/imageFunctions";
 import { CategoriaDAO } from "../dao/CategoriaDAO";
@@ -40,7 +41,7 @@ const AgregarManualidadScreen = () => {
                         doc.data().nombre
                     );
                     categorias.push(categoria);
-                    
+
                 });
 
                 setCategorias(categorias);
@@ -57,7 +58,7 @@ const AgregarManualidadScreen = () => {
                         doc.data().cantidadStock,
                         doc.data().precio
                     );
-                    materiasPrimas.push({obj: materiaPrima, seleccionado: false});
+                    materiasPrimas.push({ obj: materiaPrima, seleccionado: false });
                 });
                 setMateriasPrimas(materiasPrimas);
             }
@@ -65,36 +66,36 @@ const AgregarManualidadScreen = () => {
 
         return () => { subCategorias(), subMateriasPrimas() }
     }, []);
-    
+
     const handleChangeText = (attribute, value) => {
-        setManualidad({ ...manualidad, [attribute]: value})
+        setManualidad({ ...manualidad, [attribute]: value })
     }
 
     const handlePickImage = async () => {
         const img = await imageFunctions.pickImage();
         setImage(img)
     };
-    
+
     const handleInsert = async () => {
         const nuevoProducto = await ProductoDAO.findByName(manualidad.nombre);
-        if (!nuevoProducto){
+        if (!nuevoProducto) {
             const filename = manualidad.nombre + "%" + manualidad.categoria + "%" + manualidad.descripcion;
 
-            const producto = new Producto(null, manualidad.nombre, manualidad.descripcion, filename, 
+            const producto = new Producto(null, manualidad.nombre, manualidad.descripcion, filename,
                 manualidad.categoria, manualidad.precio, manualidad.cantidadStock);
-            
+
             seleccionados.forEach(matPrima => {
                 producto.addMateriaPrima(matPrima.obj.getId)
             });
-            
+
             const registro = await ProductoDAO.insert(producto);
             if (registro) {
                 setUploading(true);
                 await imageFunctions.uploadImage(image, filename);
                 setUploading(false);
                 setImage(null);
-            } 
-            
+            }
+
         } else {
             Alert.alert('Manualidad Existente', `La manualidad con nombre ${manualidad.nombre} ya existe`)
         }
@@ -102,7 +103,7 @@ const AgregarManualidadScreen = () => {
 
     const handleSelection = (id) => {
         const listaActualizada = materiasPrimas.map((matPrima) => {
-            if( matPrima.obj.getId === id) {
+            if (matPrima.obj.getId === id) {
                 return {
                     obj: matPrima.obj,
                     seleccionado: !matPrima.seleccionado
@@ -111,37 +112,37 @@ const AgregarManualidadScreen = () => {
                 return matPrima;
             };
         });
-        setMateriasPrimas(listaActualizada); 
+        setMateriasPrimas(listaActualizada);
 
         const seleccionadosActuales = listaActualizada.filter((matPrima) => matPrima.seleccionado);
         setSeleccionados(seleccionadosActuales);
     }
-          
+
     return (
         <ScrollView style={estilos.containerScrollView}>
             <View style={estilos.container}>
-            
+
                 <TextInput placeholder="Nombre" inputMode="text"
-                    onChangeText={(value) => handleChangeText("nombre", value)} 
+                    onChangeText={(value) => handleChangeText("nombre", value)}
                     style={estilos.input} />
 
                 <TextInput placeholder="Descripción" inputMode="text"
-                    onChangeText={(value) => handleChangeText("descripcion", value)} 
+                    onChangeText={(value) => handleChangeText("descripcion", value)}
                     style={estilos.input} />
 
                 <TextInput placeholder="Precio" inputMode="decimal"
-                    onChangeText={(value) => handleChangeText("precio", value)} 
+                    onChangeText={(value) => handleChangeText("precio", value)}
                     style={estilos.input} />
 
                 <TextInput placeholder="Cantidad" inputMode="numeric"
-                    onChangeText={(value) => handleChangeText("cantidadStock", value)} 
+                    onChangeText={(value) => handleChangeText("cantidadStock", value)}
                     style={estilos.input} />
 
-                <Picker onValueChange={(value) => {handleChangeText("categoria", value)}}
+                <Picker onValueChange={(value) => { handleChangeText("categoria", value) }}
                     style={estilos.picker}>
                     <Picker.Item label={"Categoría"} />
                     {categorias.map((categoria) => (
-                        <Picker.Item key={categoria.getId} label={categoria.getNombre} 
+                        <Picker.Item key={categoria.getId} label={categoria.getNombre}
                             value={categoria.getId} />
                     ))}
                 </Picker>
@@ -149,26 +150,26 @@ const AgregarManualidadScreen = () => {
                 <TouchableOpacity style={estilos.botonHome} onPress={handlePickImage}>
                     <Text style={estilos.buttonText}>Seleccionar Imagen</Text>
                 </TouchableOpacity>
-                
+
                 <View >
-                    {image && <Image source={{uri: image.uri}} style={{width: 150, height: 150, alignSelf:"center", marginBottom:10}}/>}
+                    {image && <Image source={{ uri: image.uri }} style={{ width: 150, height: 150, alignSelf: "center", marginBottom: 10 }} />}
                 </View>
-        
+
                 <TouchableOpacity style={estilos.botonHome} onPress={() => setModalVisible(true)}>
                     <Text style={estilos.buttonText}>Seleccionar Materias Primas</Text>
                 </TouchableOpacity>
 
                 <Modal animationType="slide" transparent={false}
-                    visible={modalVisible} 
+                    visible={modalVisible}
                     onRequestClose={() => setModalVisible(false)} >
-                    
+
                     {materiasPrimas.map((matPrima) => (
                         <View style={estilos.containerCheck} key={"v" + matPrima.obj.getId}>
                             <CheckBox key={"c" + matPrima.obj.getId} value={matPrima.seleccionado}
                                 onValueChange={() => handleSelection(matPrima.obj.getId)} />
                             <Text style={estilos.buttonText} key={"t" + matPrima.obj.getId}>{matPrima.obj.getNombre}</Text>
                         </View>
-                        
+
                     ))}
                     <TouchableOpacity style={estilos.botonHome}
                         onPress={() => setModalVisible(false)}>
